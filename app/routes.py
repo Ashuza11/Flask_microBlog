ee50d72b82bd6be46b5c26448087820dd60d35ee
M ash <ashuza1411@gmail.com>
2022-05-12 16:44:51 +0200

    Create database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from turtle import title
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
+from app.models import User, Post
 from app.forms import LoginForm
 
 @app.route('/')
@@ -27,3 +28,7 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('/index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
